13ed0bbe48bf57c082730c96f51c42fa6095170a
DamirPavlik <damir.pavlik@gmail.com>
2025-01-15 22:14:35 +0100

    refactor: add variables

--- src/toolbar-actions.ts
+++ src/toolbar-actions.ts
@@ -1,27 +1,32 @@
 import { drawPaths } from './draw-paths.js';
 
+const clear = document.querySelector("#clear") as HTMLButtonElement;
+const undo = document.querySelector("#undo") as HTMLButtonElement;
+const redo = document.querySelector("#redo") as HTMLButtonElement;
+const save = document.querySelector("#save") as HTMLButtonElement;
+
 export function setupToolbarActions(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, points: PathData[], redoPoints: PathData[]): void {
-    document.querySelector("#clear")!.addEventListener("click", () => {
+    clear.addEventListener("click", () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         points.length = 0;
         redoPoints.length = 0;
     });
 
-    document.querySelector("#undo")!.addEventListener("click", () => {
+    undo.addEventListener("click", () => {
         if (points.length > 0) {
             redoPoints.push(points.pop()!);
             drawPaths(ctx, points);
         }
     });
 
-    document.querySelector("#redo")!.addEventListener("click", () => {
+    redo.addEventListener("click", () => {
         if (redoPoints.length > 0) {
             points.push(redoPoints.pop()!);
             drawPaths(ctx, points);
         }
     });
 
-    document.querySelector("#save")!.addEventListener("click", () => {
+    save.addEventListener("click", () => {
         const dataUrl = canvas.toDataURL("image/png", 1.0);
         const a = document.createElement("a");
         a.href = dataUrl;
