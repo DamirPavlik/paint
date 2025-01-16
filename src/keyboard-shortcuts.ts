4428f82de541b2a2a7f8054b1863ba259d2d2769
DamirPavlik <damir.pavlik@gmail.com>
2025-01-16 22:21:42 +0100

    feat: add shortcuts

---
+++ src/keyboard-shortcuts.ts
@@ -0,0 +1,18 @@
+export function setupKeyboardShortcuts(undo: () => void, redo: () => void) { 
+    window.addEventListener("keydown", function(e) {
+        if (e.ctrlKey || e.metaKey) {
+            console.log("tu sme");
+            if (e.key === "z") {
+                console.log("undoooo");
+                e.preventDefault();
+                undo();
+            }
+
+            if (e.key === "y") {
+                console.log("redoooo");
+                e.preventDefault();
+                redo();
+            }
+        }
+    });
+}
