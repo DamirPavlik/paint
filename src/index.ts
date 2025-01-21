9b20a909c008dc76c1c03f6d072d9a4810884353
DamirPavlik <damir.pavlik@gmail.com>
2025-01-21 13:40:40 +0100

    refactor: add file upload init to main file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { setupCanvas } from './canvas-setup.js';
 import { setupCanvasEvents } from './event-handlers.js';
+import setupFileUpload from './file-upload.js';
 import { setupKeyboardShortcuts } from './keyboard-shortcuts.js';
 import { redoHandler, setupToolbarActions, undoHandler } from './toolbar-actions.js';
 
@@ -7,7 +8,9 @@ const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const color = document.querySelector("#color") as HTMLInputElement;
 const thickness = document.querySelector("#thickness") as HTMLInputElement;
+const uploadImage = document.querySelector("#uploadImage") as HTMLInputElement;
 
+let baseImage: HTMLImageElement | null = null;
 let points: PathData[] = [];
 let redoPoints: PathData[] = [];
 let selectedBrush = "circle";
@@ -16,27 +19,6 @@ setupCanvas();
 setupCanvasEvents(canvas, ctx, points, redoPoints, selectedBrush, thickness, color);
 setupToolbarActions(ctx, canvas, points, redoPoints);
 setupKeyboardShortcuts(undoHandler, redoHandler);
-
-// will refactor later
-const uploadImage = document.querySelector("#uploadImage") as HTMLInputElement;
-let baseImage: HTMLImageElement | null = null;
-
-uploadImage.addEventListener("change", function (e) {
-    const file = (e.target as HTMLInputElement).files?.[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = function (event) {
-        baseImage = new Image();
-        baseImage.onload = function () {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(baseImage!, 0, 0, canvas.width, canvas.height);
-
-            redoPoints.length = 0;
-        };
-        baseImage.src = event.target?.result as string;
-    };
-    reader.readAsDataURL(file);
-});
+setupFileUpload(canvas, ctx, redoPoints, baseImage, uploadImage)
 
 export { canvas, baseImage, points, redoPoints }
